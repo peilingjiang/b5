ba2d09ac55b58a92294404a8925e453a7201cd48
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2020-09-22 01:51:04 -0400

    fix test

--- src/__test__/components/editor/editor.test.js
+++ src/__test__/components/editor/editor.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 
-import Editor from '../../../components/editor/editor'
+import { Editor } from '../../../components/editor/editor'
 
 describe('Editor', () => {
   test('renders without crash', () => {
