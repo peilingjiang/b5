f61870dfd91c918c475368ca8465c2a32eeb186c
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2021-07-09 01:58:37 -0400

    update ga

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-import ReactGA from 'react-ga'
-
-// Google Analysis
-ReactGA.initialize('UA-133943899-3')
-ReactGA.pageview(window.location.pathname + window.location.search)
-
 ReactDOM.render(
   <StrictMode>
     <App />
